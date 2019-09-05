c5e98bc7536e342348917992be8f8d5609ad19f6
Aaron L. Zeng <me@bcc32.com>
2019-09-05 14:39:46 -0400

    Enable automatic formatting

--- plugin.ml
+++ plugin.ml
@@ -1,11 +1,14 @@
 open Ecaml
 
 let () =
-  defun (Symbol.intern "ecaml-hello") [%here] (Returns Value.Type.unit)
- (let open Defun.Let_syntax in
-  let%map_open () = return () in
-  message "Hello from OCaml";
-  ())
+  defun
+    (Symbol.intern "ecaml-hello")
+    [%here]
+    (Returns Value.Type.unit)
+    (let open Defun.Let_syntax in
+     let%map_open () = return () in
+     message "Hello from OCaml";
+     ())
 ;;
 
 let () = provide (Symbol.intern "ecaml-hello")
